abf5332a496e08d6e3ea88a61225bc808cb29ec3
Orlando Rodriguez <orlandogrodriguez94@gmail.com>
2025-03-06 13:04:03 -0500

    Initial Commit

---
+++ XMLCompare/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  XMLCompare
+//
+//  Created by Orlando Rodriguez  on 3/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XMLCompare/XMLCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  XMLCompareApp.swift
+//  XMLCompare
+//
+//  Created by Orlando Rodriguez  on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct XMLCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
